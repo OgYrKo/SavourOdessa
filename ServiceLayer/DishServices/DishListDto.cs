576b57b3689d64d32722495880b1c81e23d7b08b
Maksym Galchynskyi <mgalchinskyi@gmail.com>
2024-02-06 15:13:24 +0100

    feat: Add Dish and Restaurant Dto

---
+++ ServiceLayer/DishServices/DishListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceLayer.DishServices
+{
+    public class DishListDto
+    {
+        public int Dishid { get; set; }
+        public string Dishname { get; set; } = null!;
+        public string Dishtype { get; set; } = null!;
+        public TimeSpan Preparingtime { get; set; }
+        public string Dishkitchen { get; set; } = null!;
+        public string Photo { get; set; } = null!;
+        public decimal AverageRating { get; set; }
+    }
+}
---
+++ ServiceLayer/RestaurantServices/RestaurantListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceLayer.RestaurantServices
+{
+    public class RestaurantListDto
+    {
+        public int Restaurantid { get; set; }
+        public string Restaurantname { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string? Photo { get; set; }
+        public double AverageRating { get; set; }
+        public bool IsVerified { get; set; }
+
+    }
+}
