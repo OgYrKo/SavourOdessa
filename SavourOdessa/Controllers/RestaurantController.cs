047d0d00f1eab3de9a7bf9a9d2561b7c8aac9c9c
Maksym Galchynskyi <mgalchinskyi@gmail.com>
2024-03-01 12:05:07 +0100

    ???

---
+++ SavourOdessa/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.EfClasses;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.RestaurantServices;
+using ServiceLayer.RestaurantServices.Concrete;
+
+namespace SavourOdessa.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private readonly DataContext _context;
+        public RestaurantController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(SortFilterPageOptions options)
+        {
+            var listService = new ListRestaurantsService(_context);
+            var restaurantList = listService.SortFilterPage(options).ToList();
+            return View(new RestaurantListCombinedDto(options,restaurantList));
+        }
+    }
+}
---
+++ ServiceLayer/RestaurantServices/RestaurantListCombinedDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.RestaurantServices
+{
+    public class RestaurantListCombinedDto
+    {
+        public RestaurantListCombinedDto(SortFilterPageOptions sortFilterPageData, IEnumerable<RestaurantListDto> restaurantList)
+        {
+            SortFilterPageData = sortFilterPageData;
+            RestaurantList = restaurantList;
+        }
+
+        public SortFilterPageOptions SortFilterPageData { get; private set; }
+
+        public IEnumerable<RestaurantListDto> RestaurantList { get; private set; }
+    }
+}
