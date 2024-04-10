e57f514647c779d34a70b30f4f8dbb780389ff4f
Maksym Galchynskyi <mgalchinskyi@gmail.com>
2024-04-10 18:36:07 +0200

    feat: Add Manager

---
+++ SavourOdessa/Areas/Manager/Models/Restaurants/RestaurantEditViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace SavourOdessa.Areas.Manager.Models.Restaurants
+{
+    public class RestaurantEditViewModel
+    {
+        public int RestaurantId { get; set; }
+        public string RestaurantName { get; set; } = null!;
+        public CityItemViewModel[]? Cities { get; set; } = null!;
+        public int SelectedCityId { get; set; }
+        public CountryItemViewModel[]? Countries { get; set; } = null!;
+        public int SelectedCountryId { get; set; }
+        //add regex for street with ukrainian and english letters, first letter should be capital
+        [RegularExpression(@"^[A-ZА-Я][a-zA-Zа-яА-Я\s]*$", ErrorMessage = "Street should contain only letters (first letter should be capital)")]
+        public string Street { get; set; } = null!;
+
+        [RegularExpression(@"^\d+[a-zA-Zа-яА-Я]*$", ErrorMessage = "HouseNumber should start with a digit and can contain letters.")]
+        public string HouseNumber { get; set; } = null!;
+        public RepeatRuleViewModel[]? RepeatRules { get; set; } = null!;
+        public List<TimeRuleViewModel>? TimeRules { get; set; } = null!;
+    }
+    public class CityItemViewModel
+    {
+        public int CityId { get; set; }
+        public int CountryId { get; set; }
+        public string CityName { get; set; } = null!;
+    }
+    public class CountryItemViewModel
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; } = null!;
+    }
+}
---
+++ SavourOdessa/Areas/Manager/Models/Restaurants/RestaurantListItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SavourOdessa.Areas.Manager.Models.Restaurants
+{
+    public class RestaurantListItemViewModel
+    {
+        public int RestaurantId { get; set; }
+        public string RestaurantName { get; set; } = null!;
+    }
+}
